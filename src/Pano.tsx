e5205279419caa901f3dd1eab350835ea460b77c
krosthw <crosta93@gmail.com>
2022-04-24 17:17:00 +0200

    second

--- src/Pano.tsx
+++ src/Pano.tsx
@@ -1,23 +1,21 @@
 import * as PANOLENS from "panolens";
 import imageSrc from './assets/pano3.jpg';
 console.log(PANOLENS);
+console.log(imageSrc);
 const panorama = new PANOLENS.ImagePanorama(imageSrc);
 console.log("panorama");
 console.log(panorama);
-// const viewer = new PANOLENS.Viewer({
-//     container: document.querySelector("#coucou")
-// });
-// console.log(viewer);
-// viewer.add(panorama);
+const viewer = new PANOLENS.Viewer({
+    container: document.querySelector("#coucou")
+});
+viewer.add(panorama);
 const Pano = () => {
-    const viewer = new PANOLENS.Viewer({
-        container: document.querySelector("#coucou")
-    });
-    viewer.add(panorama);
+
     return (
         <>
             <p>Coucou</p>
             <div id="coucou" >                
+            {}
             </div>
         </>
     );
