3cc70f2ddd3507b54161381c5d836063c51fb5bc
krosthw <crosta93@gmail.com>
2022-05-01 11:10:08 +0200

    some changes

--- src/Pano.tsx
+++ src/Pano.tsx
@@ -2,6 +2,7 @@
 
 
 import dat from "dat-gui";
+import React from "react";
 import * as PANOLENS from "panolens";
 import * as THREE from "three";
 import * as TWEEN from "tween.js";
@@ -9,14 +10,16 @@ import imageSrc from './assets/pano3.jpg';
 console.log(PANOLENS);
 console.log(imageSrc);
 var panorama = new PANOLENS.ImagePanorama(imageSrc);
+var viewer = new PANOLENS.Viewer({ output: 'console', container: document.querySelector('#pcontainer') });
+viewer.add(panorama);
+// viewer.add(panorama, panorama_video);
+viewer.renderer.sortObjects = true;
 //console.log("panorama");
 //console.log(panorama);
 // var viewer = new PANOLENS.Viewer({
 //     container: document.querySelector("#pano")
 // });
 // viewer.add(panorama);
-
-var parameters:any, gui:any, /*panorama_video:any,*/ radius, button:any, position, infospot, timerId:any, easingItem:any, list, listItem, index = 0, baseScale = 300;
 
 
 function getPropertyArray(object: any) {
@@ -40,90 +43,78 @@ function iterativeFocus(enabled: any) {
 }
 
 function onFocus() {
-
     this.focus(parameters.duration, TWEEN.Easing[parameters.curve][parameters.easing]);
-
 }
 
-const Pano = () => {
+var parameters:any, gui:any, /*panorama_video:any,*/ radius, button:any, position, infospot, timerId:any, easingItem:any, list, listItem, index = 0, baseScale = 300;
 
-    var curves = [], easings = [];
-    curves = getPropertyArray(TWEEN.Easing);
-    list = document.querySelector('.mdl-list');
-    listItem = document.querySelector('.mdl-list__item');
+class Pano extends React.Component {
+//const Pano = () => {
 
-    // Focus tweening parameter
-    parameters = {
-        amount: 50,
-        duration: 1000,
-        curve: 'Exponential',
-        easing: 'Out',
-        iterative: false
-    };
+    render() {
 
-    gui = new dat.GUI();
-    gui.add(parameters, 'duration', 0, 3000).step(50);
-    gui.add(parameters, 'iterative').onChange(iterativeFocus);
-    gui.add(parameters, 'curve', curves).onChange(function (value :any) {
-        gui.remove(easingItem);
-        easings = getPropertyArray(TWEEN.Easing[value]);
-        parameters.easing = easings.length > 2 ? easings[1] : easings[0];
-        easingItem = gui.add(parameters, 'easing', easings);
-    });
-    easingItem = gui.add(parameters, 'easing', ['In', 'Out', 'InOut']);
+        console.log("Prova");
+        var curves = [], easings = [];
+        curves = getPropertyArray(TWEEN.Easing);
+        list = document.querySelector('.mdl-list');
+        listItem = document.querySelector('.mdl-list__item');
 
-    //panorama = new PANOLENS.ImagePanorama('asset/textures/equirectangular/field.jpg');
-    //panorama_video = new PANOLENS.VideoPanorama('asset/textures/video/1941-battle-low.mp4');
-    //var panorama = new PANOLENS.ImagePanorama(imageSrc);
-    panorama = new PANOLENS.ImagePanorama('assets/pano3.jpg');
-    //panorama.link(panorama_video, new THREE.Vector3(3883.71, 745.13, -3047.48), 400, 'asset/textures/1941-battle-thumb.png');
-    //panorama_video.link(panorama, new THREE.Vector3(4602.58, 1863.13, -547.70), 400);
+        // Focus tweening parameter
+        parameters = {
+            amount: 50,
+            duration: 1000,
+            curve: 'Exponential',
+            easing: 'Out',
+            iterative: false
+        };
 
-    const { edgeLength } = panorama;
-    radius = edgeLength / 2;
+        // gui = new dat.GUI();
+        // gui.add(parameters, 'duration', 0, 3000).step(50);
+        // gui.add(parameters, 'iterative').onChange(iterativeFocus);
+        // gui.add(parameters, 'curve', curves).onChange(function (value :any) {
+        //     gui.remove(easingItem);
+        //     easings = getPropertyArray(TWEEN.Easing[value]);
+        //     parameters.easing = easings.length > 2 ? easings[1] : easings[0];
+        //     easingItem = gui.add(parameters, 'easing', easings);
+        // });
+        // easingItem = gui.add(parameters, 'easing', ['In', 'Out', 'InOut']);
 
-    for (var i = 1; i <= parameters.amount; i++) {
+        //panorama = new PANOLENS.ImagePanorama('asset/textures/equirectangular/field.jpg');
+        //panorama_video = new PANOLENS.VideoPanorama('asset/textures/video/1941-battle-low.mp4');
+        //var panorama = new PANOLENS.ImagePanorama(imageSrc);
+        //panorama = new PANOLENS.ImagePanorama('assets/pano3.jpg');
+        //panorama.link(panorama_video, new THREE.Vector3(3883.71, 745.13, -3047.48), 400, 'asset/textures/1941-battle-thumb.png');
+        //panorama_video.link(panorama, new THREE.Vector3(4602.58, 1863.13, -547.70), 400);
 
-        const theta = Math.random() * 2 * Math.PI;
-        const phi = Math.random() * Math.PI;
+        const { edgeLength } = panorama;
+        radius = edgeLength / 2;
 
-        position = new THREE.Vector3(
-            radius * Math.sin(phi) * Math.cos(theta),
-            radius * Math.sin(phi) * Math.sin(theta),
-            radius * Math.cos(phi)
-        );
+        for (var i = 1; i <= parameters.amount; i++) {
 
-        infospot = new PANOLENS.Infospot(baseScale * position.length() / radius, PANOLENS.DataImage.Info);
-        infospot.position.copy(position);
-        infospot.addHoverText('Infospot' + i);
-        infospot.addEventListener('click', onFocus);
-        panorama.add(infospot);
+            const theta = Math.random() * 2 * Math.PI;
+            const phi = Math.random() * Math.PI;
 
-        // Add to left panel
-        var item = listItem!.cloneNode(true) as any;
-        item.classList.remove('template');
-        item.querySelector('button').textContent = 'Infospot' + i;
-        item.addEventListener('click', onFocus.bind(infospot), false);
-        list!.appendChild(item);
+            position = new THREE.Vector3(
+                radius * Math.sin(phi) * Math.cos(theta),
+                radius * Math.sin(phi) * Math.sin(theta),
+                radius * Math.cos(phi)
+            );
 
-    }
-    // var viewer = new PANOLENS.Viewer({
-    //     container: document.querySelector("#pano")
-    // });
-    // viewer.add(panorama);
-   var viewer = new PANOLENS.Viewer({ output: 'console', container: document.querySelector('#pcontainer') });
-    viewer.add(panorama);
-    // viewer.add(panorama, panorama_video);
-    viewer.renderer.sortObjects = true;
+            infospot = new PANOLENS.Infospot(baseScale * position.length() / radius, PANOLENS.DataImage.Info);
+            infospot.position.copy(position);
+            infospot.addHoverText('Infospot' + i);
+            infospot.addEventListener('click', onFocus);
+            panorama.add(infospot);
 
-    return (
-        // <div>
-        //     <p>Costantini Alessandro</p>
-        //     <div id="pano" >
-        //     {}
-        //     </div>
-        //     <p>Credits</p>
-        // </div>
+            // Add to left panel
+            // var item = listItem!.cloneNode(true) as any;
+            // item.classList.remove('template');
+            // item.querySelector('button').textContent = 'Infospot' + i;
+            // item.addEventListener('click', onFocus.bind(infospot), false);
+            // list!.appendChild(item);
+
+        }
+        return (
         <div className="container">
             <span className="left mdl-layout mdl-layout--fixed-header">
                 <header className="mdl-layout__header">
@@ -140,6 +131,7 @@ const Pano = () => {
             <span id="pcontainer" className="right"></span>
         </div>
     );
+    }
 };
 
 export default Pano;
